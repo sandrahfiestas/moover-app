232b3e6a98a3462079429d2a2d181291dec98399
sandrahfiestas <shfiestas@gmail.com>
2021-03-06 22:09:05 -0500

    instalando dependencias

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/scss/main.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -34,9 +35,17 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/style-resources'
   ],
+
+  styleResources: {
+    scss: [
+      'assets/scss/_variables.scss'
+    ]
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
+
 }
