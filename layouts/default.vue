ea293690a44762c9cf3285f5f412b62c6fef6d04
sandrahfiestas <shfiestas@gmail.com>
2021-03-08 08:54:05 -0500

    Importando Header y Footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,43 @@
 <template>
   <div>
-    <Nuxt />
+    <Header />
+    <main>
+      <Nuxt />
+    </main>
+    <Footer />
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<script>
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+export default {
+  components: {
+    Header,
+    Footer
+  },
+  data: () => {
+    return {
+      slides: [
+        {
+          image: 'https://i.ibb.co/Kwpf1LM/Abstract-decorative-blue-wave-banner-design-vector.jpg',
+          text: 'Imagen Uno'
+        },
+        {
+          image: 'https://i.ibb.co/jDy4F1t/Abstract-stylish-wave-banner-design-vector.jpg',
+          text: 'Imagen Dos'
+        },
+        {
+          image: 'https://i.ibb.co/f9QyPkD/Abstract-pink-color-wave-design-banner-vector.jpg',
+          text: 'Imagen Tres'
+        }
+      ]
+    }
+  }
 }
+</script>
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+<style lang="scss">
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
